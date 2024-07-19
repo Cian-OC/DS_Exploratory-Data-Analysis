4587d1808a31e26045c66814a480baca231d0438
Cian-OC <cianoc09@gmail.com>
2024-07-19 12:49:18 +0100

    Add files via upload
    Code for plot 2

---
+++ plot2.R
@@ -0,0 +1,39 @@
+
+
+## Plot 2: Linegraph of Global Active Power and Time
+
+## Uses same subset of household_power_consumption, First_Feb_2007, created for plot 1.
+
+## Create a column of date and time merged
+
+dateTime <- strptime(paste(First_Feb_2007$Date, First_Feb_2007$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+
+First_Feb_2007 <- cbind(First_Feb_2007, dateTime)
+
+## Correct column classes
+
+First_Feb_2007$Date <- as.Date(First_Feb_2007$Date, format = "%d/%M/%Y")
+
+First_Feb_2007$Time <- format(First_Feb_2007$Time, format="%H:%M:%S")
+
+First_Feb_2007$Global_active_power <- as.numeric(First_Feb_2007$Global_active_power)
+
+First_Feb_2007$Global_reactive_power <- as.numeric(First_Feb_2007$Global_reactive_power)
+
+First_Feb_2007$Voltage <- as.numeric(First_Feb_2007$Voltage)
+
+First_Feb_2007$Global_intensity <- as.numeric(First_Feb_2007$Global_intensity)
+
+First_Feb_2007$Sub_metering_1 <- as.numeric(First_Feb_2007$Sub_metering_1)
+
+First_Feb_2007$Sub_metering_2 <- as.numeric(First_Feb_2007$Sub_metering_2)
+
+First_Feb_2007$Sub_metering_3 <- as.numeric(First_Feb_2007$Sub_metering_3)
+
+
+## Plot the data
+
+png("plot2.png", width=480, height=480)
+with(plot(First_Feb_2007$dateTime,First_Feb_2007$Global_active_power, type="l", xlab="Day", ylab="Global Active Power (kilowatts)"))
+dev.off()
+
